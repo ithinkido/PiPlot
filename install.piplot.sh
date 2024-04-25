12622e1d7bfa25b07701de5634259bd7f83bbc21
ithinkido <52834821+ithinkido@users.noreply.github.com>
2024-04-25 14:58:37 +0200

    g

--- install.piplot.sh
+++ install.piplot.sh
@@ -22,32 +22,24 @@ echo "Updating apt. This could take a while ..."
 wait
 sudo apt-get -y upgrade -qq > /dev/null) & spinner
 wait
-
-DIR="/boot/firmware"
-if [ ! -d "$DIR" ]; then
-    DIR="/boot"
+if ! grep -q '#flow control serial' '/boot/config.txt' ; then
+    echo $'\n#flow control serial' | sudo tee -a /boot/config.txt >/dev/null
 fi
-
-if ! grep -q '#flow control serial' "$DIR/config.txt"; then
-    echo $'\n#flow control serial' | sudo tee -a "$DIR/config.txt" >/dev/null
-fi
-if ! grep -q 'dtoverlay=disable-bt' "$DIR/config.txt"; then
-    echo "dtoverlay=disable-bt" | sudo tee -a "$DIR/config.txt" >/dev/null
+if ! grep -q 'dtoverlay=disable-bt' '/boot/config.txt' ; then
+    echo "dtoverlay=disable-bt" | sudo tee -a /boot/config.txt >/dev/null   
 fi    
-if ! grep -q 'enable_uart=1' "$DIR/config.txt"; then
-    echo "enable_uart=1" | sudo tee -a "$DIR/config.txt" >/dev/null
+if ! grep -q 'enable_uart=1' '/boot/config.txt' ; then
+    echo "enable_uart=1" | sudo tee -a /boot/config.txt >/dev/null
     echo ""
     printf "UART 1 Enabled \n"
 fi    
-if ! grep -q 'dtoverlay=uart-ctsrts'"$DIR/config.txt"; then
-    echo "dtoverlay=uart-ctsrts" | sudo tee -a "$DIR/config.txt" >/dev/null
+if ! grep -q 'dtoverlay=uart-ctsrts' '/boot/config.txt' ; then
+    echo "dtoverlay=uart-ctsrts" | sudo tee -a /boot/config.txt >/dev/null
     echo ""
     printf "CTS RTS Device tree enabled \n"
 fi
-sudo sed -i -E 's/console\s*=\s*\w+\s*,\s*[0-9]*//g' "$DIR/cmdline.txt"
-
+sudo sed -i -E 's/console\s*=\s*\w+\s*,\s*[0-9]*//g' /boot/cmdline.txt
 sudo wget -q -c -P /boot/overlays https://raw.githubusercontent.com/ithinkido/PiPlot/main/uart-ctsrts.dtbo
-sudo wget -q -c -P /boot/firmware/overlays https://raw.githubusercontent.com/ithinkido/PiPlot/main/uart-ctsrts.dtbo
 sudo systemctl -q stop serial-getty@ttyAMA0.service
 sudo systemctl disable serial-getty@ttyAMA0.service
 echo ""
